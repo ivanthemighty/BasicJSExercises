aca196890bfc054bf84329b308df967833989024
ivanthemighty <74321181+ivanthemighty@users.noreply.github.com>
2021-02-01 18:56:18 +0100

    Add files via upload
    I added the bonus on the 3rd exercise 
    Its not perfect but it should work

--- app.js
+++ app.js
@@ -50,49 +50,73 @@
 
 // let input = parseInt(prompt('Insert Coin'));
 // let input = 376;
-
 // function changeCalculator(amount) {
 //     let array = [];
 //     let newTotal = 0;
 //     if(amount%50 !=0) {
+//         let limit = 2500;
 //         let reminder = 0;
 //         reminder = parseInt(amount/50);
+//         if(limit/reminder > 0)
+//         {
 //         // console.log(reminder);
 //         newTotal = amount - (reminder*50);
 //         // console.log(newTotal);
 //         array.push(reminder + "x50 Coins");
+//         }else {
+//             console.log('This machine has reached the maximum');
+//         }
 //     }
 //     if(newTotal%20 !=0) {
+//         let limit = 1600;
 //         let reminder = 0;
 //         reminder = parseInt(newTotal/20);
+//         if(limit/reminder > 0) {
 //         // console.log(reminder);
 //         newTotal = newTotal - (reminder*20);
 //         // console.log(newTotal);
 //         array.push(reminder + "x20 Coins")
+//         }else {
+//             console.log('This machine has reached the maximum');
+//         }
 //     }
-//     if(newTotal%20 !=0) {
+//     if(newTotal%10 !=0) {
+//         let limit = 2000;
 //         let reminder = 0;
 //         reminder = parseInt(newTotal/10);
+//         if(limit/reminder > 0) {
 //         // console.log(reminder);
 //         newTotal = newTotal - (reminder*10);
 //         // console.log(newTotal);
 //         array.push(reminder + "x10 Coins")
+//         } else {
+//             console.log('This machine has reached the maximum');
+//         }
 //     }
-//     if(newTotal%20 !=0) {
+//     if(newTotal%5 !=0) {
+//         let limit = 2500;
 //         let reminder = 0;
 //         reminder = parseInt(newTotal/5);
+//         if(limit/reminder > 0) {
 //         // console.log(reminder);
 //         newTotal = newTotal - (reminder*5);
 //         // console.log(newTotal);
 //         array.push(reminder + "x5 Coins")
+//         }else {
+//             console.log('This machine has reached the maximum');
+//         }
 //     }
-//     if(newTotal%20 !=0) {
+//     if(newTotal%1 !=0) {
 //         let reminder = 0;
 //         reminder = parseInt(newTotal/1);
+//         if(limit/reminder > 0) {
 //         // console.log(reminder);
 //         newTotal = newTotal - (reminder*1);
 //         // console.log(newTotal);
 //         array.push(reminder + "x1 Coins")
+//         }else {
+//             console.log('This machine has reached the maximum');
+//         }
 //     }
 //     console.log(array);
 // }
